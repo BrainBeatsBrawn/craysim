b3703e749e0b2fd54ea4b678550d695300702ebb
Seb James <sebj_coder3@pm.me>
2026-08-05 14:59:40 +0100

    Switch to defining a single macro

--- cmake/module_definitions.cmake
+++ cmake/module_definitions.cmake
@@ -1,7 +1,8 @@
 #
 # Define variables of module groups for use by client projects.
 #
-macro(setup_module_variables_for_craysim_maths maths_directory json_directory)
+macro(setup_module_variables_for_craysim craysim_directory mathplot_directory maths_directory json_directory)
+
   set(CRAYSIM_MATHS_MODULES
     ${maths_directory}/sm/hdfdata.cppm
     ${maths_directory}/sm/config.cppm
@@ -28,35 +29,31 @@ macro(setup_module_variables_for_craysim_maths maths_directory json_directory)
     ${maths_directory}/sm/config.cppm
     ${json_directory}/src/modules/json.cppm
   )
-endmacro()
 
-macro(setup_module_variables_for_craysim_mathplot base_directory)
   set(CRAYSIM_MATHPLOT_MODULES
-    ${base_directory}/mplot/fps/profiler.cppm
-    ${base_directory}/mplot/compoundray/interop.cppm
-    ${base_directory}/mplot/compoundray/Ommatidium.cppm
-    ${base_directory}/mplot/jcvoronoi/jc_voronoi.cppm
-    ${base_directory}/mplot/compoundray/EyeVisual.cppm
-    ${base_directory}/mplot/VerticesVisual.cppm
-    ${base_directory}/mplot/NormalsVisual.cppm
-    ${base_directory}/mplot/InstancedScatterVisual.cppm
+    ${mathplot_directory}/mplot/fps/profiler.cppm
+    ${mathplot_directory}/mplot/compoundray/interop.cppm
+    ${mathplot_directory}/mplot/compoundray/Ommatidium.cppm
+    ${mathplot_directory}/mplot/jcvoronoi/jc_voronoi.cppm
+    ${mathplot_directory}/mplot/compoundray/EyeVisual.cppm
+    ${mathplot_directory}/mplot/VerticesVisual.cppm
+    ${mathplot_directory}/mplot/NormalsVisual.cppm
+    ${mathplot_directory}/mplot/InstancedScatterVisual.cppm
   )
   set(CRAYSIM_MATHPLOT_DOUBLEHEX_MODULES
-    ${base_directory}/mplot/ScatterVisual.cppm
-    ${base_directory}/mplot/QuiverVisual.cppm
-    ${base_directory}/mplot/HexGridVisual.cppm
-    ${base_directory}/mplot/LengthscaleVisual.cppm
+    ${mathplot_directory}/mplot/ScatterVisual.cppm
+    ${mathplot_directory}/mplot/QuiverVisual.cppm
+    ${mathplot_directory}/mplot/HexGridVisual.cppm
+    ${mathplot_directory}/mplot/LengthscaleVisual.cppm
   )
-endmacro()
 
-macro(setup_module_variables_for_craysim base_directory)
   set(CRAYSIM_MODULES
-    ${base_directory}/modules/craysim_visual.cppm
+    ${craysim_directory}/modules/craysim_visual.cppm
   )
   set(CRAYSIM_DOUBLEHEX_MODULES
-    ${base_directory}/modules/craysim_doublehexgrid.cppm
+    ${craysim_directory}/modules/craysim_doublehexgrid.cppm
   )
   set(CRAYSIM_ANTBODY_MODULES
-    ${base_directory}/modules/craysim_antbody.cppm
+    ${craysim_directory}/modules/craysim_antbody.cppm
   )
 endmacro()
